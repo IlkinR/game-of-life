16505fbeb953ef6787493627c51833caa8b9bbaf
Ilkin Ramazanov <babalke44@gmail.com>
2021-06-12 03:26:14 +0400

    setuped game file and required global vartiables was created

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+#include <unistd.h>
+
+#define MAX_ROWS_COUNT 50
+#define MAX_COLUMNS_COUNT 50
+typedef int Universe[MAX_ROWS_COUNT][MAX_COLUMNS_COUNT];
 
 int main() {
-   printf("Everything is ok");
    return 0;
 }
