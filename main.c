1b5b1654aaed16a651f7a3c2b6527d26d2654fa7
Ilkin Ramazanov <babalke44@gmail.com>
2021-06-12 03:28:05 +0400

    added function for counting neighbours in a unuiverse for a given cell

--- main.c
+++ main.c
@@ -8,6 +8,30 @@
 #define MAX_COLUMNS_COUNT 50
 typedef int Universe[MAX_ROWS_COUNT][MAX_COLUMNS_COUNT];
 
+int getTotalNeighboursCount(Universe universe, int rowInput, int colInput)
+{
+    int neighboursCount = 0;
+
+    for (int i = rowInput - 1; i <= rowInput + 1; i++)
+    {
+        for (int j = colInput - 1; j <= colInput + 1; j++)
+        {
+            bool isCurrentCell = rowInput == i && colInput == j;
+
+            int neighbourRow = (i + MAX_ROWS_COUNT) % MAX_ROWS_COUNT;
+            int neighbourColumn = (j + MAX_COLUMNS_COUNT) % MAX_COLUMNS_COUNT;
+            bool isNeighbour = universe[neighbourRow][neighbourColumn];
+
+            if (!isCurrentCell && isNeighbour)
+            {
+                neighboursCount++;
+            }
+        }
+    }
+
+    return neighboursCount;
+}
+
 int main() {
    return 0;
 }
